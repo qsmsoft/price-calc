615d9ca5ab32981b3eafb69d1356a8bdd98b49e5
qsmsoft <qsmsoft@gmial.com>
2024-06-04 18:07:39 +0500

    basic version of the program

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	prices := []float64{10.0, 20.0, 30.0}
+	taxRates := []float64{0, 0.7, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(result)
 }
